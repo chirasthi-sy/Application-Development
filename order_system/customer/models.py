4fba6b4f183fe344280075b0bade60f46c55f919
Nathali <user@exam>
2023-11-03 14:24:21 +0530

    fixed errors

--- order_system/customer/models.py
+++ order_system/customer/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+
 class MenuItem(models.Model):
     name=models.CharField(max_length=100)
-    description=model.TextField()
+    description=models.TextField()
     image=models.ImageField(upload_to='menu_images/')
-    price=model.DecimalField(max_digits=5,decimal_places=2)
+    price=models.DecimalField(max_digits=5,decimal_places=2)
     category=models.ManyToManyField('Category',related_name='item')
 
     def __str__(self):
@@ -16,8 +17,8 @@ class Category(models.Model):
 
 class OrderModel(models.model):
     created_on=models.DateTimeField(auto_now_add=True)
-    price=modls.DecimalField(max_digits=7,decimal_places=2)
-    items=model.ManyToManyField('MenuItem',related_name='order',blank=True)
+    price=models.DecimalField(max_digits=7,decimal_places=2)
+    items=models.ManyToManyField('MenuItem',related_name='order',blank=True)
 
     def __str__(self):
         return f'Order:{self.created_on.strftime("%b %d %I:%M %p")}'
