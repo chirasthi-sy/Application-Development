a6739c3cd11be86d7601ce56acd65a09dc85529d
Nathali <user@exam>
2023-11-03 14:26:07 +0530

    fixed errors

--- order_system/customer/models.py
+++ order_system/customer/models.py
@@ -15,7 +15,7 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
-class OrderModel(models.model):
+class OrderModel(models.Model):
     created_on=models.DateTimeField(auto_now_add=True)
     price=models.DecimalField(max_digits=7,decimal_places=2)
     items=models.ManyToManyField('MenuItem',related_name='order',blank=True)
