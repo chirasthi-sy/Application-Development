5b6b5d8c828702a45801c9f95c851f4f67db7573
Nathali <user@exam>
2023-11-03 14:19:23 +0530

    fixed errors

--- order_system/customer/models.py
+++ order_system/customer/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-class MenuItem(models.model):
+class MenuItem(models.Model):
     name=models.CharField(max_length=100)
     description=model.TextField()
     image=models.ImageField(upload_to='menu_images/')
