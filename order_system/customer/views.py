a3de87d098919ddc9bb1ee141c56b709df3814e2
chirasthi-sy <chirasthi.sy@gmail.com>
2023-11-04 18:26:51 +0530

    Linked all the html files as needed in each page

--- order_system/customer/views.py
+++ order_system/customer/views.py
@@ -2,15 +2,15 @@ from django.shortcuts import render
 from django.views import View
 
 class Index(View):
-    def get(selfself,request,*args,**kwargs):
+    def get(self,request,*args,**kwargs):
         return render(request,'customer/Home_page.html')
 
 class About(View):
-    def get(selfself, request,*args, **kwargs):
+    def get(self, request,*args, **kwargs):
         return render(request, 'customer/about_us.html')
 
 class Order(View):
-    def get(selfself,request,*args,**kwargs):
+    def get(self,request,*args,**kwargs):
 
 #get every item from each category
 #pass into context
