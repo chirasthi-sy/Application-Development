acdb3c27b5a1cb06fdada5ab2dfb289fe91970d1
Nathali <user@exam>
2023-11-10 11:38:02 +0530

    #customer admin category created

--- order_system/customer/admin.py
+++ order_system/customer/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-
+from .models import Menuitem,Category,OrderModel
 # Register your models here.
 
 from .models import Category
 
 admin.site.register(Category)
+admin.site.register(MenuItem)
+admin.site.register(OrderModel)
 
