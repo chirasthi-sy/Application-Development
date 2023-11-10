87da246af6c1ce473ef9ddd2b9350e2fe390b244
chirasthi-sy <chirasthi.sy@gmail.com>
2023-11-10 15:38:48 +0530

    changed file

--- order_system/customer/views.py
+++ order_system/customer/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import View
+from .models import MenuItem, Category, OrderModel
 
 class Index(View):
     def get(self, request, *args, **kwargs):
