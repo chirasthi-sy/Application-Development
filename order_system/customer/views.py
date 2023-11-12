086bebc8783034d6b291911ece92a6770ea3aabd
chirasthi-sy <chirasthi.sy@gmail.com>
2023-11-12 14:38:14 +0530

    Modifying the menu page and order confirmation page to be dynamic for user input

--- order_system/customer/views.py
+++ order_system/customer/views.py
@@ -30,37 +30,36 @@ class Order(View):
 
         return render(request,"customer/menu.html", context)
 
-def post(self, request, *args, **kwargs):
-
-    order_items = {
-        'items': []
-    }
-
-    items = request.POST.getlist("items[]")
-    for item in items:
-        menu_item = MenuItem.objects.get(pk__contains=int(item))
-        item_data = {
-            'id': menu_item.pk,
-            'name': menu_item.name,
-            'price': menu_item.price
-
+    def post(self, request, *args, **kwargs):
+        order_items = {
+            'items': []
         }
 
-        order_items['items'].append(item_data)
-        price=0
-        item_ids=[]
+        items = request.POST.getlist("items[]")
+        for item in items:
+            menu_item = MenuItem.objects.get(pk__contains=int(item))
+            item_data = {
+                'id': menu_item.pk,
+                'name': menu_item.name,
+                'price': menu_item.price
 
-    for item in order_items['items']:
-        price += item['price']
-        item_ids.append(item['id'])
+            }
 
-    order = OrderModel.objects.create(price=price)
-    order.items.add(*item_ids)
-    context = {
-        'items': order_items['items'],
-        'price': price
-    }
-    return render(request,'order confirmation message.html', context)
+            order_items['items'].append(item_data)
+            price=0
+            item_ids=[]
+
+        for item in order_items['items']:
+            price += item['price']
+            item_ids.append(item['id'])
+
+        order = OrderModel.objects.create(price=price)
+        order.items.add(*item_ids)
+        context = {
+            'items': order_items['items'],
+            'price': price
+        }
+        return render(request, 'customer/order confirmation message.html', context)
 
 
 
