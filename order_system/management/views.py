c89448479066e3ebb83878c93ef0cc282df6ff31
chirasthi-sy <chirasthi.sy@gmail.com>
2023-11-19 13:55:21 +0530

    modified dashboard view to handle additional operations

--- order_system/management/views.py
+++ order_system/management/views.py
@@ -8,5 +8,15 @@ from customer.models import OrderModel
 class Dashboard(View):
     def get(self, request, *args, **kwargs):
         today = datetime.today()
-        orders = OrderModel.objects.filter()
-        return render(request, 'management/dashboard.html')
+        orders = OrderModel.objects.filter(
+            created_on__year=today.year, created_on__month=today.month, created_on__day=today.day)
+        total_revenue = 0
+        for order in orders:
+            total_revenue += order.price
+
+        context = {
+            'orders': orders,
+            'total_revenue': total_revenue,
+            'total_orders': len(orders)
+        }
+        return render(request, 'management/dashboard.html', context)
