44c5d416db49c5769f812f51c0cfe49609b383f0
Dilshan <sachindilshan793@gmail.com>
2023-12-06 11:59:48 +0530

    Improving readabilty of urls.py

--- order_system/management/urls.py
+++ order_system/management/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import Dashboard, OrderDetails
-
+#This creates the url patterns for the managemnet dashboard and order details view
 urlpatterns = [
     path('dashboard/', Dashboard.as_view(), name='dashboard'),
     path("orders/<int:pk>/", OrderDetails.as_view(), name='order-details'),
